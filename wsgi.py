826929d66df1d587db70b595ebc845a0369c7ad8
Preeti18nanda <preetinanda0418@gmail.com>
2023-04-24 12:39:59 +0530

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,14 @@
-from __test__ import app as application
+import os
+import sys
+
+# Add the parent directory of this file to the sys.path to make sibling packages importable
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'swagger_server')))
+
+# Import the app module from the all_methods.swagger_server.test package
+from swagger_server.test import BaseTestCase
+app = BaseTestCase().create_app()
 
 if __name__ == "__main__":
-    application.run()
+    # Start the Flask development server
+    app.run()
+
